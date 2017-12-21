357a291f738e61aa3ce1c0529000865cf879bc7d
Ko-stant <kostant.stanton@gmail.com>
2017-12-21 16:56:52 -0500

    Game board check for used square
    Updated the function to not allow for the same square to be used
    if it is already taken.

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -11,21 +11,23 @@ const isEven = function (i) {
   }
 }
 const boardArray = ['', '', '', '', '', '', '', '', '']
-
+// turnCount is set to 0 inside of function to do a check each time the function is called. The function takes in the index value from events.js and uses that as the array index for board array. Depending on the even/odd value of the turnCount the array is updated with the correct string.
 const updateCell = function (cellIndex) {
-  let count = 0
+  let turnCount = 0
   for (let i = 0; i < boardArray.length - 1; i++) {
     if (boardArray[i] === 'x' || boardArray[i] === 'o') {
-      count++
+      turnCount++
     }
   }
-  if (isEven(count) === true) {
+  if (boardArray[cellIndex] === 'x' || boardArray[cellIndex] === 'o') {
+    console.log('square is already taken.')
+  } else if (isEven(turnCount) === true) {
     boardArray[cellIndex] = 'x'
-  } else {
+  } else if (isEven(turnCount) === false) {
     boardArray[cellIndex] = 'o'
   }
   console.log(boardArray)
-  console.log(count)
+  console.log(turnCount)
 }
 
 module.exports = {
